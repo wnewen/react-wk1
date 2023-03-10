cab24df8049092727593a998b4f4c07c4166974f
wnewen <plpl541616@gmail.com>
2023-03-10 17:37:03 +0800

    模組化元件

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header"
+import ImageList from "../components/ImageList"
+import Description from "../components/Descriptioin"
+import Footer from "../components/Footer"
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <ImageList />
+            <Description />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
